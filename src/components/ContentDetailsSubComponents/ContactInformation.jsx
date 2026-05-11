49f55d781335cd1470c203625d11b530cef32bba
Gabriel Gaddi <gabriel.gaddi93@gmail.com>
2026-05-11 13:33:03 -0700

    Added resume and contact information

--- src/components/ContentDetailsSubComponents/ContactInformation.jsx
+++ src/components/ContentDetailsSubComponents/ContactInformation.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Resume from '../../assets/Gabriel Gaddi Resume.pdf';
 import './ContactInformation.css';
 
 function ContactInformation() {
@@ -8,7 +9,23 @@ function ContactInformation() {
     <div class="container">
       <div class="row">
           <div class="col-lg-12 text-left">
-            <p>Contact Information</p>
+            <h1>Contact Information</h1>
+            <p>
+              Feel free to contact me at my email or phone number.
+            </p>
+              <li><i class="fa fa-phone fa-fw"></i> (559) 824-0194</li>
+              <li><i class="fa fa-envelope-o fa-fw"></i> <a href="mailto:gabriel.gaddi93@gmail.com">gabriel.gaddi93@gmail.com</a></li>
+            <p>
+              <a href={Resume} target ="_blank"> Click here to view my resume </a>
+              <ul class="list-inline">
+                  <li>
+                      <a href="https://www.linkedin.com/in/gabrielgaddi"><i class="fa fa-linkedin fa-fw fa-3x"></i></a>
+                  </li>
+                  <li>
+                      <a href="https://www.twitch.tv/brixgale"><i class="fa fa-twitch fa-fw fa-3x"></i></a>
+                  </li>
+              </ul>
+            </p>
           </div>
       </div>
     </div>
--- src/components/ContentDetailsSubComponents/SelfSummary.jsx
+++ src/components/ContentDetailsSubComponents/SelfSummary.jsx
@@ -9,6 +9,7 @@ function SelfSummary() {
     <div class="container">
       <div class="row">
           <div class="col-lg-12 text-left">
+              <h1>Self Summary</h1>
               <img src={profilePic} alt="profile" height="384" width="216" />
               <p>I am a passionate software developer with a Bachelor's Degree in Computer Science from the University of California, San Diego. I've collaborated with different teammates to develop extravagant applications and websites, working on both front-end and back-end development.</p>
               <br />
