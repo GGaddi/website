5fd4c24e21af4c8123e7cdac96eb55558c3de5c1
Gabriel Gaddi <gabriel.gaddi93@gmail.com>
2026-05-11 17:33:07 -0700

    Added hobbies details

--- src/components/ContentDetailsSubComponents/Hobbies.jsx
+++ src/components/ContentDetailsSubComponents/Hobbies.jsx
@@ -8,7 +8,13 @@ function Hobbies() {
     <div class="container">
       <div class="row">
           <div class="col-lg-12 text-left">
-            <p>Hobbies</p>
+            <h1>Hobbies</h1>
+            <p>For anyone first meeting me, the first thing they will easily learn is that I love video games and anime. Both things are pretty much my whole life.
+              I like to stream on twitch when I can when playing and even do a few speedruns when I have the time. I also cosplay both from video games and anime such as cosplaying Sora from Kingdom Hearts
+              and Tanjiro from Demon Slayer. If you would like to see my socials for my video game streaming or recordings, feel free to check my links out!
+            </p>
+            <p><a href="https://twitch.tv/brixgale" target="_blank">Twitch</a></p>
+            <p><a href="https://youtube.com/@brixgale" target="_blank">Youtube</a></p>
           </div>
       </div>
     </div>
--- src/components/ContentDetailsSubComponents/SelfSummary.jsx
+++ src/components/ContentDetailsSubComponents/SelfSummary.jsx
@@ -10,7 +10,7 @@ function SelfSummary() {
       <div class="row">
           <div class="col-lg-12 text-left">
               <h1>Self Summary</h1>
-              <img src={profilePic} alt="profile" height="384" width="216" />
+              <img src={profilePic} alt="" height="384" width="216" />
               <p>I am a passionate software developer with a Bachelor's Degree in Computer Science from the University of California, San Diego. I've collaborated with different teammates to develop extravagant applications and websites, working on both front-end and back-end development.</p>
               <br />
               <p>As a software developer, I want to make programs exciting and fun to use. My goal is to develop software that is beneficial, but also engaging and exciting.</p>
