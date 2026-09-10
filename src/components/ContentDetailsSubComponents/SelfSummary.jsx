c5ebb94d789c10289c654739808c86bca79cd9fa
Gabriel Gaddi <gabriel.gaddi93@gmail.com>
2026-09-10 03:47:01 -0700

    Updated wordiing in self summary to match job title and updated resume to show tenure job position rather than split

--- src/components/ContentDetailsSubComponents/SelfSummary.jsx
+++ src/components/ContentDetailsSubComponents/SelfSummary.jsx
@@ -13,8 +13,8 @@ function SelfSummary({fadeInClass}) {
           <div class="col-lg-12 text-left">
               <h1>Self Summary</h1>
               <img src={profilePic} alt="" height="384" width="216" />
-              <p>I am a passionate software developer with a Bachelor's Degree in Computer Science from the University of California, San Diego. I've collaborated with different teammates to develop extravagant applications and websites, working on both front-end and back-end development.</p>
-              <p>As a software developer, I want to make programs exciting and fun to use. My goal is to develop software that is beneficial, but also engaging and exciting.</p>
+              <p>I am a passionate software engineer with a Bachelor's Degree in Computer Science from the University of California, San Diego. I've collaborated with different teammates to develop extravagant applications and websites, working on both front-end and back-end development.</p>
+              <p>As a software engineer, I want to make programs exciting and fun to use. My goal is to develop software that is beneficial, but also engaging and exciting.</p>
           </div>
       </div>
     </div>
